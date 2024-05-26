8599d04f2108ced1135c38b045ba493a2ce49ba9
MikhailKup <mifffa.mk@gmail.com>
2024-05-26 10:38:32 +0500

    Commit 1

---
+++ src/components/heroesFilters/HeroesFilters.js
@@ -0,0 +1,58 @@
+import { useEffect } from 'react';
+import { useHttp } from '../../hooks/http.hook';
+import { useDispatch, useSelector } from 'react-redux';
+import { filtersFetching, filtersFetched, filtersFetchingError, activeFilterChanged } from '../../actions/index';
+import Spinner from '../spinner/Spinner';
+import classNames from 'classnames';
+
+const HeroesFilters = () => {
+    const dispatch = useDispatch();
+    const {request} = useHttp();
+    const {filters, filtersLoadingStatus, activeFilter} = useSelector(state => state);
+
+    useEffect(() => {
+        dispatch(filtersFetching());
+        request("http://localhost:3001/filters")
+            .then(data => dispatch(filtersFetched(data)))
+            .catch(() => dispatch(filtersFetchingError()))
+            // eslint-disable-next-line
+    }, []);
+
+    if (filtersLoadingStatus === 'loading') {
+        return <Spinner/>
+    } else if (filtersLoadingStatus === 'error') {
+        return <h5 className="text-center mt-5">Ошибка загрузки</h5>
+    }
+
+    const renderFiltersList = (arr) => {
+        if (arr.length === 0) {
+            return <h5 className="text-center mt-5">Фильтры не найдены</h5>
+        }
+
+        return arr.map(({name, className, label}) => {
+            const btnClass = classNames('btn', className, {'active' : name === activeFilter});
+            return <button 
+                    key={name}
+                    id={name}
+                    className={btnClass}
+                    onClick={() => dispatch(activeFilterChanged(name))}
+                    >
+                    {label}
+                    </button>
+        })
+    }
+    const elements = renderFiltersList(filters);
+    
+    return (
+        <div className="card shadow-lg mt-4">
+            <div className="card-body">
+                <p className="card-text">Отфильтруйте героев по элементам</p>
+                <div className="btn-group">
+                    {elements}
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default HeroesFilters;
